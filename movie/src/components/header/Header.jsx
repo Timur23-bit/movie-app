0c3aa8869d0425947043dd79edd6af71ff6aec84
Timur23-bit <62034798+Timur23-bit@users.noreply.github.com>
2020-10-07 14:09:09 +0300

    2.2.15

--- movie/src/components/header/Header.jsx
+++ movie/src/components/header/Header.jsx
@@ -12,6 +12,11 @@ export default class HeaderMovie extends Component {
     page: 1
   };
 
+  constructor() {
+    super();
+    this.getRatedGuestSession();
+  }
+
   getRatedGuestSession () {
     movies
       .getRatedGuestSession(sessionStorage.sessionId)
@@ -19,30 +24,27 @@ export default class HeaderMovie extends Component {
         this.setState({
           rated: res.results,
           pageTotal: res.total_pages,
-          page: res.page
+          page: res.page,
+          flag: true
         })
       })
   };
 
-  Rated () {
-    const {rated, page, pageTotal } = this.state;
-    if (!rated) {
-      this.getRatedGuestSession();
-      return <ItemList rated={rated} pages={page} pagesTotal={pageTotal} rate/>
-    }
-    return <ItemList rated={rated} pages={page} pagesTotal={pageTotal} rate/>
-  }
-
   render() {
-    const rated = this.Rated(true);
+    const {rated, page, pageTotal, flag} = this.state;
+    const rateds = <ItemList rated={rated} pages={page} pagesTotal={pageTotal} rate/>;
 
     return (
-      <Tabs defaultActiveKey="1" onChange={() => {}} centered>
+      <Tabs defaultActiveKey="1" onChange={(key) => {
+        if (key == 2) {
+          this.getRatedGuestSession();
+        }
+      }} centered>
         <TabPane tab="Search" key="1">
           <ItemList/>
         </TabPane>
         <TabPane tab="Rated" key="2">
-          {rated}
+          {rateds}
         </TabPane>
       </Tabs>
     )
